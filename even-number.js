6700421f17276633239ce14c82a65643d7a7c7be
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2026-01-16 08:15:12 +0530

    Program to check number is even or odd

---
+++ even-number.js
@@ -0,0 +1,11 @@
+// Program to check number is even or odd
+function evn(n) {
+  if (n % 2 == 0) {
+    return `"${n}" is even`;
+  } else {
+    // return "Number is odd";
+    return `'${n}' is odd`;
+  }
+}
+let result = evn(5);
+console.log(result);
