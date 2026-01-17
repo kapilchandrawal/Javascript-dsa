fe0863056acfcd902824f44c6d4d861af2abfcd9
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2026-01-17 13:42:57 +0530

    Program to check even number

---
+++ even.number.js
@@ -0,0 +1,10 @@
+// Program to check number is even or odd
+function evn(n) {
+  if (n % 2 == 0) {
+    return `"${n}" is even`;
+  } else {
+    return `'${n}' is odd`;
+  }
+}
+let result = evn(5);
+console.log(result);
